ccea3bf623ce08cca783439351305dd1bdb06544
JusticeV452 <55002917+JusticeV452@users.noreply.github.com>
2024-10-09 14:49:35 -0400

    Fixed page links site-wide

--- src/index.js
+++ src/index.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./scss/Home.scss";
-import './scss/index.scss';
-import Home from './Home';
-import Projects from './Projects';
-import DevLogs from './DevLogs';
-import reportWebVitals from './reportWebVitals';
-import SiteNav from './components/SiteNav';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import "./scss/index.scss";
+import Home from "./Home";
+import Projects from "./Projects";
+import DevLogs from "./DevLogs";
+import reportWebVitals from "./reportWebVitals";
+import SiteNav from "./components/SiteNav";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <SiteNav />
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function SiteRouter() {
+  // Page not auto scrolling to element after entering from another page
+  // https://github.com/vercel/next.js/issues/11109#issuecomment-751429015
+  React.useEffect(() => {
+    const path = window.location.hash;
+    if (path && path.includes("#")) {
+      setTimeout(() => {
+        const id = path.replace("#", "");
+        const el = window.document.getElementById(id);
+        if (!el) {
+          window.location.href = "/projects";
+        }
+        const navbar = window.document.getElementById("site-nav");
+        const rect = el.getBoundingClientRect();
+        window.top.scroll({
+          top: window.scrollY + rect.top - navbar.clientHeight,
+          behavior: "smooth",
+        });
+      }, 600);
+    }
+  });
+
+  return (
     <Router>
       {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+        renders the first one that matches the current URL. */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
@@ -23,6 +49,13 @@ root.render(
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
+  );
+}
+
+root.render(
+  <React.StrictMode>
+    <SiteNav />
+    <SiteRouter />
   </React.StrictMode>
 );
 
